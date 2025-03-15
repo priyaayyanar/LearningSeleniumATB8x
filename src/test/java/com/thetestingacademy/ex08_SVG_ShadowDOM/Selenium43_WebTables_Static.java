4ac2e84e737bb88379ce4a385239ea83f13e9107
Priya Ayyanar <priyamcanandha@gmail.com>
2025-03-15 11:42:30 +0530

    WebTables- Completed

--- src/test/java/com/thetestingacademy/ex08_SVG_ShadowDOM/Selenium43_WebTables_Static.java
+++ src/test/java/com/thetestingacademy/ex08_SVG_ShadowDOM/Selenium43_WebTables_Static.java
@@ -31,7 +31,7 @@ public class Selenium43_WebTables_Static {
         int row = driver.findElements(By.xpath("//table[@id='customers']/tbody/tr")).size();
         System.out.println(row);
         // To find Column - //table[@id='customers]/tbody/tr[2]/td
-        int column = driver.findElements(By.xpath("//table[@id='customers]/tbody/tr[2]/td//table[@id='customers']/tbody/tr[2]/td")).size();
+        int column = driver.findElements(By.xpath("//table[@id='customers']/tbody/tr[2]/td")).size();
         System.out.println(column);
 
         // Logic
@@ -50,13 +50,29 @@ public class Selenium43_WebTables_Static {
         String firstPart = "//table[@id='customers']/tbody/tr[";
         String secondPart = "]/td[";
         String thirdPart = "]";
+        //table[@id='customers]/tbody/tr[2]/td//table[@id='customers']/tbody/tr[2]/td
 
         for (int i=2; i<=row; i++){
             for (int j=1; j<=column; j++){
                 String dynamic_xPath = firstPart + i + secondPart + j + thirdPart ;
                 String data = driver.findElement(By.xpath(dynamic_xPath)).getText();
-                System.out.println(data);
+                System.out.print((data)+"  |  ");
+
+                if(data.contains("Maria Anders")){
+                    String country_path = dynamic_xPath+"/following-sibling::td";
+                    String company_path = dynamic_xPath+"/preceding-sibling::td";
+                    String country_text = driver.findElement(By.xpath(country_path)).getText();
+                    String company_text = driver.findElement(By.xpath(company_path)).getText();
+                    System.out.println();
+                    System.out.println("------------------------------------");
+                    System.out.print(country_text);
+                    System.out.print("  |  ");
+                    System.out.print(company_text);
+                    System.out.println();
+                    System.out.println("------------------------------------");
+                }
             }
+            System.out.println();
         }
 
 
